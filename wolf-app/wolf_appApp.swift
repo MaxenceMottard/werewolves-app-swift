807be0d092bf3b8cdaba169d3c4c23ecb7c766bc
MaxenceMottard <mottard.maxence@gmail.com>
2021-02-02 21:24:32 +0100

    Initial Commit

---
+++ wolf-app/wolf_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  wolf_appApp.swift
+//  wolf-app
+//
+//  Created by Maxence Mottard on 02/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct wolf_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
